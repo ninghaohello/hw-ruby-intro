fb90f39cb1edc74c3aa8437f7f7c3b6e89675ecd
Ninghao <liuninghaoo163@gmail.com>
2019-02-05 22:30:17 -0600

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,88 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+    (arr.empty?)? 0:arr.reduce(:+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+    if arr.empty?
+        return 0
+    elsif arr.size == 1
+        return arr[0]
+    else
+        arr.sort!{|x,y| -(x <=> y)}
+        return arr[0]+arr[1]
+    end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+    if arr.size < 2
+        false
+    else
+        arr.sort!
+        i = 0
+        while i < arr.size
+            a = arr[i]
+            return false if a > n
+            toFind = n - a
+            j = i + 1
+            while j < arr.size
+                return true if (arr[j] == toFind)
+                break if (arr[j] > toFind)
+                j = j + 1
+            end
+            i = i + 1
+        end
+        false
+    end
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.empty?
+    return false
+  else
+    ss = s.downcase.chars.first     # all to lower case
+    print ss
+    if ss.match(/[a-z]/)            # is letter
+      if ss.match(/[aeiou]/)        # chars do not want
+        return false
+      else
+        return true
+      end
+    else
+      return false
+    end
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s.empty? || s.match(/\D/)      # emptry or non-digits
+    return false
+  else
+    if s.end_with?('00') || s=='0'
+      return true
+    else
+      return false
+    end
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  def initialize(isbn, price)
+    raise ArgumentError, 'Invalid..' if isbn.empty? || price <=0
+    @isbn = isbn
+    @price = price
+  end
+  def price_as_string()
+    return '$'+'%.2f'%@price
+  end
 end
